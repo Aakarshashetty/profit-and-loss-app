a47f9e2470bbbfadaec57feb036a395b119a1e61
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-08-23 16:47:12 +0530

    added css and fixed logic

--- script.js
+++ script.js
@@ -4,31 +4,37 @@ const currPrice = document.querySelector('.curr-price');
 const tellMe = document.querySelector('.tell-me-btn');
 const output = document.querySelector('.output');
 
-const calculateProfitOrLoss = (initial, current, stock) => {
-    if(initial < current){
-        const profit = (current-initial) * stock;
-        const percentage = (profit/initial)*100;
-        output.style.color = "green";
-        output.innerText = `Hey, the profit is ${profit} and the percent is ${percentage.toFixed(2)}%`;
-    }
-    else if(initial > current){
-        const loss = (initial - current)*stock;
-        const percentage = (loss/initial)*100;
+const calculateProfitOrLoss = (initial, stock, current) => {
+    console.log(initial, current, stock);
+    output.style.display = "inline";
+    if(initial > current){
+        var loss = (initial - current)*stock;
+        var percentage = (loss/initial)*100;
         output.style.color = "red";
         output.innerText = `Hey, the loss is ${loss} and the percent is ${percentage.toFixed(2)}%`;
     }
+    
+    else if(current > initial){
+        var profit = (current-initial) * stock;
+        var percentage = (profit/initial)*100;
+        output.style.color = "green";
+        output.innerText = `Hey, the profit is ${profit} and the percent is ${percentage.toFixed(2)}%`;
+    }
     else{
+        output.style.color = "black";
         output.innerText = "No pain no gain, and no gain no pain🙂";
     }
 }
 tellMe.addEventListener("click", ()=>{
-    var ip = initialPrice.value;
-    var cp = currPrice.value;
-    var st = stocks.value;
+    var ip = Number(initialPrice.value);
+    var cp = Number(currPrice.value);
+    var st = Number(stocks.value);
     if(ip && cp && st !== ''){
-        calculateProfitOrLoss(ip,cp,st);
+        calculateProfitOrLoss(ip,st,cp);
     }
     else{
+        output.style.display = "inline";
+        output.style.color = "black";
         output.innerText = "Please fill out all the fields😐"
     }
 });
